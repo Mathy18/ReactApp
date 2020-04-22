9b6362cdcc45502074a76e82c92e863b88167c5b
krishnga <ganesh.krishnamurthy@morganstanley.com>
2020-04-22 20:48:49 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar"></img>
         </a>
-      </header>
+        <div className="content">
+          <a href="/" className="author">
+            Indu
+          </a>
+          <div className="metadata"></div>
+          Today 6:00 pm
+          <div className="text">Nice blog post</div>
+        </div>
+      </div>
     </div>
   );
 }
